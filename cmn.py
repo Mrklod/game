963bb4f5cf0061293fc9e95a92554c570012eae6
Sasha Osin <sashaosin364@gmail.com>
2023-01-26 17:37:44 +0700

    update

--- cmn.py
+++ cmn.py
@@ -8,23 +8,39 @@ class Game:
         ans = input("Выбирайте:(камень,ножницы или бумага)").lower()
         c = choice(value)
         if ans == 'камень' and c == 'камень':
-            im = Image.open("img/nogniz.png")
+            im = Image.open("img/camen.jpg")
             im.show()
             print('Ничья')
         elif ans == 'камень' and c == 'ножницы':
+            im = Image.open("img/nogniz.jpg")
+            im.show()
             print('Победа')
         elif ans == 'камень' and c == 'бумага':
+            im = Image.open("img/paper.jpg")
+            im.show()
             print('Поражение')
         if ans == 'ножницы' and c == 'ножницы':
+            im = Image.open("img/nogniz.jpg")
+            im.show()
             print('Ничья')
         elif ans == 'ножницы' and c == 'камень':
+            im = Image.open("img/camen.jpg")
+            im.show()
             print('Поражение')
         elif ans == 'ножницы' and c == 'бумага':
+            im = Image.open("img/paper.jpg")
+            im.show()
             print('Победа')
         if ans == 'бумага' and c == 'бумага':
+            im = Image.open("img/paper.jpg")
+            im.show()
             print('Ничья')
         elif ans == 'бумага' and c == 'ножницы':
+            im = Image.open("img/nogniz.jpg")
+            im.show()
             print('Поражение')
         elif ans == 'бумага' and c == 'камень':
+            im = Image.open("img/camen.jpg")
+            im.show()
             print('Победа')
-        print(c)
+        print(f'Противник показал: {c}')
